8c86f8283398edf4619d5ee1972458a694c38a17
mrosefiss <mrosefiss@gmail.com>
2019-01-30 20:42:04 -0500

    Update 1.2_mbta-service-area.sql

--- 1.2_mbta-service-area.sql
+++ 1.2_mbta-service-area.sql
@@ -1,4 +1,6 @@
--- This creates a single polygon which represents the MBTA service area
+-- This creates a single polygon which represents the MBTA service area 
+-- It requires that the shapefile for the MBTA Highest Assessment Area be loaded into a table called "mbta_primary_area"
+-- and that the shapefile for the MBTA Secondary Assessment Area be loaded into a table called "mbta_secondary_area"
 CREATE TABLE mbta_service_areas AS
 SELECT ST_UNION(
 	ST_TRANSFORM(p.geom, 2163), ST_TRANSFORM(s.geom, 2163)
